4e245d1e2b7bacda31dfd67708a114fddfb87c61
Thybing <yanbinghao86@gmail.com>
2024-07-06 11:50:31 +0800

    主函数文档完成。--yanbinghao86@gmail.com

--- digital_clock/src/sw/include/process_func.h
+++ digital_clock/src/sw/include/process_func.h
@@ -1,5 +1,32 @@
 #ifndef _PROCESS_FUNC_H_
 #define _PROCESS_FUNC_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "../include/uart.h"
+#include "../include/xprintf.h"
+#include "../include/seg_show.h"
+#include "../include/sec_clock.h"
+#include "../include/button.h"
+
+enum main_status{
+    SHOW_TIME = 0,
+    SHOW_SET_TIME,
+    SHOW_ALARM_TIME
+};
+
+struct time
+{
+    uint8_t hour;
+    uint8_t min;
+    uint8_t sec;
+};
+
+void digital_clock_init();
+struct time timestamp_to_time(uint32_t timestamp);
+void seg_show_cur_time(uint32_t timestamp);
+void seg_show_tar_time(uint32_t timestamp);
+void seg_show_alarm_time(uint32_t timestamp);
 
 #endif //_PROCESS_FUNC_H_
